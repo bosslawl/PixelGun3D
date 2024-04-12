c5ec324cca4c8afe67833b443872395e2ebeeefc
boss <boss@rain.ac>
2024-04-12 16:12:41 +0100

    updated to newest patch - cant find gadgetcd

--- Cheat/Utils/Offsets.h
+++ Cheat/Utils/Offsets.h
@@ -21,81 +21,81 @@ namespace Offsets {
 
 #	// Anticheat bypass
 
-	inline uintptr_t ACOne = String2Offset(OBFUSCATE("0xB1EDF0")); // DataSystem.DataValidiation.GClass264 .cctor
-	inline uintptr_t ACTwo = String2Offset(OBFUSCATE("0xB1AF00")); // DataSystem.DataValidation.FluentValidators.Class2536 .ctor
-	inline uintptr_t ACThree = String2Offset(OBFUSCATE("0x6FDBE0")); // DveeloperConsoleController HandleBanUs
-	inline uintptr_t ACFour = String2Offset(OBFUSCATE("0xB38D90")); // GamePauseController method_3
-	inline uintptr_t ACFive = String2Offset(OBFUSCATE("0xBB0960")); // Rilisoft.GClass317 smethod_66
-	inline uintptr_t ACSix = String2Offset(OBFUSCATE("0xBAFED0")); // Rilisoft.GClass317 smethod_67
-	inline uintptr_t ACSeven = String2Offset(OBFUSCATE("0xBAC620")); // Rilisoft.GClass317 smethod_71
-	inline uintptr_t ACEight = String2Offset(OBFUSCATE("0xBC27E0")); // Rilisoft.GClass317 smethod_76
-	inline uintptr_t ACNine = String2Offset(OBFUSCATE("0xBB29A0")); // Rilisoft.GClass317 smethod_131
-	inline uintptr_t ACTen = String2Offset(OBFUSCATE("0xBAF2D0")); // Rilisoft.GClass317 smethod_157
-	inline uintptr_t ACEleven = String2Offset(OBFUSCATE("0xBB2900")); // Rilisoft.GClass317 smethod_158
-	inline uintptr_t ACTwelve = String2Offset(OBFUSCATE("0xBC73E0")); // Rilisoft.GClass317 smethod_175
-	inline uintptr_t ACThirteen = String2Offset(OBFUSCATE("0xBC38D0")); // Rilisoft.GClass317 smethod_176
-	inline uintptr_t ACFourteen = String2Offset(OBFUSCATE("0xBB62D0"));	// Rilisoft.GClass317 smethod_201
-	inline uintptr_t ACFifteen = String2Offset(OBFUSCATE("0xBAE2B0")); // Rilisoft.GClass317 smethod_61
-	inline uintptr_t ACSixteen = String2Offset(OBFUSCATE("0xBA8910")); // Rilisoft.GClass317 smethod_129
-	inline uintptr_t ACSeventeen = String2Offset(OBFUSCATE("0xBC74B0")); // Rilisoft.GClass317 smethod_180
-	inline uintptr_t ACEighteen = String2Offset(OBFUSCATE("0xBB6BA0")); // Rilisoft.GClass317 smethod_190
-	inline uintptr_t ACNineteen = String2Offset(OBFUSCATE("0xBBFC00")); // Rilisoft.GClass317 smethod_227
-	inline uintptr_t ACTwenty = String2Offset(OBFUSCATE("0xBADD70")); // Rilisoft.GClass317 smethod_16
-	inline uintptr_t ACTwentyOne = String2Offset(OBFUSCATE("0xBC4620")); // Rilisoft.GClass317 smethod_17
-	inline uintptr_t ACTwentyTwo = String2Offset(OBFUSCATE("0xBA9DD0")); // Rilisoft.GClass317 smethod_42
+	inline uintptr_t ACOne = String2Offset(OBFUSCATE("0xB16E90")); // DataSystem.DataValidiation.GClass264 .cctor
+	inline uintptr_t ACTwo = String2Offset(OBFUSCATE("0xB18F10")); // DataSystem.DataValidation.FluentValidators.Class2536 .ctor
+	inline uintptr_t ACThree = String2Offset(OBFUSCATE("0x6FE690")); // DveeloperConsoleController HandleBanUs
+	inline uintptr_t ACFour = String2Offset(OBFUSCATE("0x45D440")); // GamePauseController method_3
+	inline uintptr_t ACFive = String2Offset(OBFUSCATE("0xBAFE60")); // Rilisoft.GClass317 smethod_66
+	inline uintptr_t ACSix = String2Offset(OBFUSCATE("0xBC7AA0")); // Rilisoft.GClass317 smethod_67
+	inline uintptr_t ACSeven = String2Offset(OBFUSCATE("0xBC7370")); // Rilisoft.GClass317 smethod_71
+	inline uintptr_t ACEight = String2Offset(OBFUSCATE("0xBA7F60")); // Rilisoft.GClass317 smethod_76
+	inline uintptr_t ACNine = String2Offset(OBFUSCATE("0xBBEEB0")); // Rilisoft.GClass317 smethod_131
+	inline uintptr_t ACTen = String2Offset(OBFUSCATE("0xBC8640")); // Rilisoft.GClass317 smethod_157
+	inline uintptr_t ACEleven = String2Offset(OBFUSCATE("0xBBC220")); // Rilisoft.GClass317 smethod_158
+	inline uintptr_t ACTwelve = String2Offset(OBFUSCATE("0xBC1660")); // Rilisoft.GClass317 smethod_175
+	inline uintptr_t ACThirteen = String2Offset(OBFUSCATE("0xBB25B0")); // Rilisoft.GClass317 smethod_176
+	inline uintptr_t ACFourteen = String2Offset(OBFUSCATE("0xBC56D0"));	// Rilisoft.GClass317 smethod_201
+	inline uintptr_t ACFifteen = String2Offset(OBFUSCATE("0xBA89D0")); // Rilisoft.GClass317 smethod_61
+	inline uintptr_t ACSixteen = String2Offset(OBFUSCATE("0xBA5ED0")); // Rilisoft.GClass317 smethod_129
+	inline uintptr_t ACSeventeen = String2Offset(OBFUSCATE("0xBC1470")); // Rilisoft.GClass317 smethod_180
+	inline uintptr_t ACEighteen = String2Offset(OBFUSCATE("0xBC87C0")); // Rilisoft.GClass317 smethod_190
+	inline uintptr_t ACNineteen = String2Offset(OBFUSCATE("0xBB80C0")); // Rilisoft.GClass317 smethod_227
+	inline uintptr_t ACTwenty = String2Offset(OBFUSCATE("0xBB9D40")); // Rilisoft.GClass317 smethod_16
+	inline uintptr_t ACTwentyOne = String2Offset(OBFUSCATE("0xBC00D0")); // Rilisoft.GClass317 smethod_17
+	inline uintptr_t ACTwentyTwo = String2Offset(OBFUSCATE("0xBBC9D0")); // Rilisoft.GClass317 smethod_42
 	inline uintptr_t ACTwentyThree = String2Offset(OBFUSCATE("0xBB4C40")); // ?
-	inline uintptr_t ACTwentyFour = String2Offset(OBFUSCATE("0xBBC930")); // Rilisoft.GClass317 smethod_69
-	inline uintptr_t ACTwentyFive = String2Offset(OBFUSCATE("0xBACE80")); // Rilisoft.GClass317 smethod_24
-	inline uintptr_t ACTwentySix = String2Offset(OBFUSCATE("0xBAC620")); // Rilisoft.GClass317 smethod_71
-	inline uintptr_t ACTwentySeven = String2Offset(OBFUSCATE("0xBB7FD0")); // Rilisoft.GClass317 smethod_73
-	inline uintptr_t ACTwentyEight = String2Offset(OBFUSCATE("0xBB2FE0")); // Rilisoft.GClass317 smethod_27
-	inline uintptr_t ACTwentyNine = String2Offset(OBFUSCATE("0xBA4490")); // Rilisoft.GClass317 smethod_74
-	inline uintptr_t ACThirty = String2Offset(OBFUSCATE("0xBC1110")); // Rilisoft.GClass317 smethod_75
-	inline uintptr_t ACThirtyOne = String2Offset(OBFUSCATE("0xBB20A0")); // Rilisoft.GClass317 smethod_77
-	inline uintptr_t ACThirtyTwo = String2Offset(OBFUSCATE("0xBAB520")); // Rilisoft.GClass317 smethod_88
-	inline uintptr_t ACThirtyThree = String2Offset(OBFUSCATE("0xBB1B80")); // Rilisoft.GClass317 smethod_89
-	inline uintptr_t ACThirtyFour = String2Offset(OBFUSCATE("0xBB4940")); // Rilisoft.GClass317 smethod_103
-	inline uintptr_t ACThirtyFive = String2Offset(OBFUSCATE("0xBB7440")); // Rilisoft.GClass317 smethod_130
-	inline uintptr_t ACThirtySix = String2Offset(OBFUSCATE("0xBB29A0")); // Rilisoft.GClass317 smethod_131
-	inline uintptr_t ACThirtySeven = String2Offset(OBFUSCATE("0xBA8C80")); // Rilisoft.GClass317 smethod_36
-	inline uintptr_t ACThirtyEight = String2Offset(OBFUSCATE("0xBAB9A0")); // Rilisoft.GClass317 smethod_143
-	inline uintptr_t ACThirtyNine = String2Offset(OBFUSCATE("0xBA78A0")); // Rilisoft.GClass317 smethod_39
-	inline uintptr_t ACFourty = String2Offset(OBFUSCATE("0xBBE400")); // Rilisoft.GClass317 smethod_40
-	inline uintptr_t ACFourtyOne = String2Offset(OBFUSCATE("0xBA6650")); // Rilisoft.GClass317 smethod_41
+	inline uintptr_t ACTwentyFour = String2Offset(OBFUSCATE("0xBBB3E0")); // Rilisoft.GClass317 smethod_69
+	inline uintptr_t ACTwentyFive = String2Offset(OBFUSCATE("0xBA9010")); // Rilisoft.GClass317 smethod_24
+	inline uintptr_t ACTwentySix = String2Offset(OBFUSCATE("0xBC7370")); // Rilisoft.GClass317 smethod_71
+	inline uintptr_t ACTwentySeven = String2Offset(OBFUSCATE("0xBBE5C0")); // Rilisoft.GClass317 smethod_73
+	inline uintptr_t ACTwentyEight = String2Offset(OBFUSCATE("0xBBA210")); // Rilisoft.GClass317 smethod_27
+	inline uintptr_t ACTwentyNine = String2Offset(OBFUSCATE("0xBC9150")); // Rilisoft.GClass317 smethod_74
+	inline uintptr_t ACThirty = String2Offset(OBFUSCATE("0xBC4B60")); // Rilisoft.GClass317 smethod_75
+	inline uintptr_t ACThirtyOne = String2Offset(OBFUSCATE("0xBAE020")); // Rilisoft.GClass317 smethod_77
+	inline uintptr_t ACThirtyTwo = String2Offset(OBFUSCATE("0xBB5070")); // Rilisoft.GClass317 smethod_88
+	inline uintptr_t ACThirtyThree = String2Offset(OBFUSCATE("0xBB5DF0")); // Rilisoft.GClass317 smethod_89
+	inline uintptr_t ACThirtyFour = String2Offset(OBFUSCATE("0xBB2250")); // Rilisoft.GClass317 smethod_103
+	inline uintptr_t ACThirtyFive = String2Offset(OBFUSCATE("0xBC06C0")); // Rilisoft.GClass317 smethod_130
+	inline uintptr_t ACThirtySix = String2Offset(OBFUSCATE("0xBBEEB0")); // Rilisoft.GClass317 smethod_131
+	inline uintptr_t ACThirtySeven = String2Offset(OBFUSCATE("0xBB1F90")); // Rilisoft.GClass317 smethod_36
+	inline uintptr_t ACThirtyEight = String2Offset(OBFUSCATE("0xBC46F0")); // Rilisoft.GClass317 smethod_143
+	inline uintptr_t ACThirtyNine = String2Offset(OBFUSCATE("0xBC6360")); // Rilisoft.GClass317 smethod_39
+	inline uintptr_t ACFourty = String2Offset(OBFUSCATE("0xBB0470")); // Rilisoft.GClass317 smethod_40
+	inline uintptr_t ACFourtyOne = String2Offset(OBFUSCATE("0xBAB100")); // Rilisoft.GClass317 smethod_41
 	inline uintptr_t ACFourtyTwo = String2Offset(OBFUSCATE("0xBA9DD0")); // Rilisoft.GClass317 smethod_42
 	inline uintptr_t ACFourtyThree = String2Offset(OBFUSCATE("0xBC3EE0")); // Rilisoft.GClass317 smethod_43
 
 	// Player_move_c
 
-	inline uintptr_t PlayerMoveCUpdate = String2Offset(OBFUSCATE("0x1B61D20")); // Player_move_c Update
-	inline uintptr_t XRay = String2Offset(OBFUSCATE("0x1AF82B0")); // Player_move_c method_205
-	inline uintptr_t MatchInvisibility = String2Offset(OBFUSCATE("0x1ABF8D0")); // Player_move_c MakeInvisibleForSeconds
-	inline uintptr_t JetpackFly = String2Offset(OBFUSCATE("0x1B17E60")); // Player_move_c method_159
-	inline uintptr_t GodmodeOne = String2Offset(OBFUSCATE("0x1AF9130")); // Player_move_c method_451
+	inline uintptr_t PlayerMoveCUpdate = String2Offset(OBFUSCATE("0x1B677D0")); // Player_move_c Update
+	inline uintptr_t XRay = String2Offset(OBFUSCATE("0x1B290B0")); // Player_move_c method_205
+	inline uintptr_t MatchInvisibility = String2Offset(OBFUSCATE("0x1AC54B0")); // Player_move_c MakeInvisibleForSeconds
+	inline uintptr_t JetpackFly = String2Offset(OBFUSCATE("0x1AD22B0")); // Player_move_c method_159
+	inline uintptr_t GodmodeOne = String2Offset(OBFUSCATE("0x1AD8570")); // Player_move_c method_451
 
 	// WeaponSounds
 
-	inline uintptr_t WeaponSoundsUpdate = String2Offset(OBFUSCATE("0x7EF390")); // WeaponSounds Update
-	inline uintptr_t NextHitCritical = String2Offset(OBFUSCATE("0x7E84E0"));// WeaponSounds SetNextHitCritical
+	inline uintptr_t WeaponSoundsUpdate = String2Offset(OBFUSCATE("0x7F0070")); // WeaponSounds Update
+	inline uintptr_t NextHitCritical = String2Offset(OBFUSCATE("0x7E91C0"));// WeaponSounds SetNextHitCritical
 
 	// PetEngine
 
-	inline uintptr_t PetEngineUpdate = String2Offset(OBFUSCATE("0xDDB840")); // Rilisoft.PetEngine Update
-	inline uintptr_t PetHealth = String2Offset(OBFUSCATE("0xDD5F90")); // PetEngine method_37
-	inline uintptr_t PetDamage = String2Offset(OBFUSCATE("0x213A620")); // PetInfo get_Attack
-	inline uintptr_t PetSpeed = String2Offset(OBFUSCATE("0x213AAB0")); // PetInfo get_SpeedModif
+	inline uintptr_t PetEngineUpdate = String2Offset(OBFUSCATE("0xDFD690")); // Rilisoft.PetEngine Update
+	inline uintptr_t PetHealth = String2Offset(OBFUSCATE("0xDD7B20")); // PetEngine method_37
+	inline uintptr_t PetDamage = String2Offset(OBFUSCATE("0x213E3D0")); // PetInfo get_Attack
+	inline uintptr_t PetSpeed = String2Offset(OBFUSCATE("0x213E860")); // PetInfo get_SpeedModif
 
 	// Misc
 
-	inline uintptr_t SpeedModifier = String2Offset(OBFUSCATE("0x774D40")); // ItemRecord get_SpeedModifier
-	inline uintptr_t GodmodeTwo = String2Offset(OBFUSCATE("0x217C6C0")); // GClass147 method_16
-	inline uintptr_t InfiniteGems = String2Offset(OBFUSCATE("0x4BB8D0")); // Class9 smethod_6
-	inline uintptr_t ReloadModifier = String2Offset(OBFUSCATE("0x72CC90")); // Class1837 smethod_13
-	inline uintptr_t GadgetCooldown = String2Offset(OBFUSCATE("0x1E846F0")); // Class835 get_Single_2
-	inline uintptr_t Lottery = String2Offset(OBFUSCATE("0x783EA0")); // Class1877 get_Int32_0
-	inline uintptr_t FireRate = String2Offset(OBFUSCATE("0x113B040")); // BalanceController smethod_82
-	inline uintptr_t MaxLevelOne = String2Offset(OBFUSCATE("0x7531F0")); // ExperienceController get_Int32_0
-	inline uintptr_t MaxLevelTwo = String2Offset(OBFUSCATE("0x753FA0")); // ExperienceController get_Int32_1
+	inline uintptr_t SpeedModifier = String2Offset(OBFUSCATE("0x7757F0")); // ItemRecord get_SpeedModifier
+	inline uintptr_t GodmodeTwo = String2Offset(OBFUSCATE("0x2180FC0")); // GClass147 method_16
+	inline uintptr_t InfiniteGems = String2Offset(OBFUSCATE("0x4BBE80")); // Class9 smethod_6
+	inline uintptr_t ReloadModifier = String2Offset(OBFUSCATE("0x729120")); // Class1837 smethod_13
+	inline uintptr_t GadgetCooldown = String2Offset(OBFUSCATE("0x1E846F0")); // Class835 get_Single_2 - ????
+	inline uintptr_t Lottery = String2Offset(OBFUSCATE("0x781F00")); // Class1877 get_Int32_0
+	inline uintptr_t FireRate = String2Offset(OBFUSCATE("0x111B350")); // BalanceController smethod_82
+	inline uintptr_t MaxLevelOne = String2Offset(OBFUSCATE("0x752C60")); // ExperienceController get_Int32_0
+	inline uintptr_t MaxLevelTwo = String2Offset(OBFUSCATE("0x7530A0")); // ExperienceController get_Int32_1
 }
 
 namespace FieldOffsets {
